9200fc6b758e2b453fed66dd911bee5bc60f7c22
Nerthust <danilotoro978@gmail.com>
2024-05-04 15:22:09 -0500

    Fix json file generator

--- distilbert-dataset/src/__main__.py
+++ distilbert-dataset/src/__main__.py
@@ -1,10 +1,13 @@
 import sys
 import yaml
 import pandas as pd
-import uuid
 from datetime import datetime
 import json
 import argparse
+import os
+
+json_categories = {"0": "Neutral"}
+json_metadata = {}
 
 def get_config_data(path):
     data = None
@@ -14,14 +17,13 @@ def get_config_data(path):
 
 def save_to_dataframe(text_list, category, df):
     data = pd.DataFrame({
-        "id": [uuid.uuid4() for _ in text_list],
         "text": text_list,
         "category": [category] * len(text_list)
     })
     return pd.concat([df, data], ignore_index=True)
 
-def save(config, random_neutral, random_medical_condition, df):
-    df = save_to_dataframe(random_neutral, 4, df)
+def save_classification(config, random_neutral, random_medical_condition, df):
+    df = save_to_dataframe(random_neutral, 0, df)
     df = save_to_dataframe(random_medical_condition, config["category"], df)
 
     return df
@@ -41,47 +43,58 @@ def classify_dataset(dataset, config):
 
     return neutral_text, medical_condition_text
 
-def save_to_json(file_name, json_obj):
-    with open(file_name,'r+') as file:
-        file_data = json.load(file)
-        file_data["metadata"].append(json_obj)
-        file.seek(0)
-        json.dump(file_data, file, indent = 4)
+def save_to_json(json_obj, utc_datetime):
+    file_name = f"./metrics/metrics__{utc_datetime}.json"
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name,'w') as json_file:
+        json.dump(json_obj, json_file, indent=4)
 
-def generate_dataset(config_datasets, file_json):
-    df = pd.DataFrame(columns=['id', 'text', 'category'])
+def save_to_tsv(df, utc_datetime):
+    file_name = f"./datasets/output__{utc_datetime}.tsv"
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    df.to_csv(file_name, sep='\t', index=False)
+
+def save(df):
+    utc_datetime = datetime.utcnow()
+    utc_datetime_str = utc_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+    json_obj = {
+        "categories": json_categories,
+        "metadata": json_metadata
+    }
+    save_to_tsv(df, utc_datetime_str)
+    save_to_json(json_obj, utc_datetime_str)
+
+
+def save_metrics(category, tag, dataset_name, metadata):
+    json_categories[category] = tag
+    json_metadata[dataset_name] = metadata
+
+def generate_dataset(config_datasets):
+    df = pd.DataFrame(columns=['text', 'category'])
     for idx in range(1, len(config_datasets["datasets"]) + 1):
-        config = config_datasets["datasets"][f"dataset{idx}"]
+        dataset_name = f"dataset{idx}"
+        config = config_datasets["datasets"][dataset_name]
         dataset = pd.read_csv(config["url"])
         neutral_text, medical_condition_text = classify_dataset(dataset, config)
 
         neutral_size = int(len(neutral_text) * config["proportion_neutral"])
         medical_condition_size = int(len(medical_condition_text) * config["proportion_with_feature"])
 
-        df = save(config, neutral_text[:neutral_size], medical_condition_text[:medical_condition_size], df)
-        json_obj = {
-            f"dataset{idx}": {
-                "neutral": 4,
-                "featured": config["category"], 
-                "total_neutral": neutral_size, 
-                "total_featured": medical_condition_size
-            }
+        df = save_classification(config, neutral_text[:neutral_size], medical_condition_text[:medical_condition_size], df)
+
+        metadata = {
+            "neutral": neutral_size,
+            "featured": medical_condition_size, 
+            "total_neutral": len(neutral_text), 
+            "total_featured": len(medical_condition_text)
         }
-
-        save_to_json(file_json, json_obj)
+        save_metrics(config["category"], config["tag_category"], dataset_name, metadata)
     
-    save_to_tsv(df)
-
-def save_to_tsv(df):
-    utc_datetime = datetime.utcnow()
-    utc_datetime_str = utc_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"./output__{utc_datetime_str}.tsv"
-    df.to_csv(file_name, sep='\t', index=False)
+    save(df)
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Generate general dataset')
     parser.add_argument('-c', help='path yaml configuration', type=str)
-    parser.add_argument('-j', help='path json', type=str)
 
     return parser
 
@@ -89,7 +102,7 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
     config_datasets = get_config_data(args.c)
-    generate_dataset(config_datasets, args.j)
+    generate_dataset(config_datasets)
 
 if __name__ == "__main__":
     sys.exit(main())
