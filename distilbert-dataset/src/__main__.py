635f3e4f39ae0026423a6fb45e92ea1425ce4efc
Nerthust <nerthust@ghostRider.myguest.virtualbox.org>
2024-05-03 20:37:49 -0500

    Definition of json

--- distilbert-dataset/src/__main__.py
+++ distilbert-dataset/src/__main__.py
@@ -3,6 +3,7 @@ import yaml
 import pandas as pd
 import uuid
 from datetime import datetime
+import json
 
 def get_config_data(path):
     data = None
@@ -42,7 +43,14 @@ def classify_dataset(dataset, config):
 
     return neutral_text, medical_condition_text
 
-def generate_dataset(config_datasets, df):
+def save_to_json(file_name, json_obj):
+    with open(file_name,'r+') as file:
+        file_data = json.load(file)
+        file_data["metadata"].append(json_obj)
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
+
+def generate_dataset(config_datasets, df, file_json):
     for idx in range(1, len(config_datasets["datasets"]) + 1):
         config = config_datasets["datasets"][f"dataset{idx}"]
         dataset = pd.read_csv(config["url"])
@@ -52,6 +60,16 @@ def generate_dataset(config_datasets, df):
         medical_condition_size = int(len(medical_condition_text) * config["proportion_with_feature"])
 
         df = save(config, neutral_text[:neutral_size], medical_condition_text[:medical_condition_size], df)
+        json_obj = {
+            f"dataset{idx}": {
+                "neutral": 4,
+                "featured": config["category"], 
+                "total_neutral": neutral_size, 
+                "total_featured": medical_condition_size
+            }
+        }
+
+        save_to_json(file_json, json_obj)
     
     return df
 
@@ -65,7 +83,7 @@ def main():
     df = pd.DataFrame(columns=['id', 'text', 'category'])
     path_config = sys.argv[1]
     config_datasets = get_config_data(path_config)
-    df = generate_dataset(config_datasets, df)
+    df = generate_dataset(config_datasets, df, 'classification.json')
     save_to_tsv(df)
 
 if __name__ == "__main__":
