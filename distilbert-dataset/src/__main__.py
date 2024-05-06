48b66b2947eed7bb22f1ed378754532eed658162
sebashack <sebastian0092@gmail.com>
2024-05-06 13:24:41 +0100

    Some refactoring and improvements

--- distilbert-dataset/src/__main__.py
+++ distilbert-dataset/src/__main__.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import yaml
 import pandas as pd
@@ -6,15 +7,8 @@ import json
 import argparse
 import os
 
-json_categories = {"0": "Neutral"}
+json_categories = {"0": "neutral"}
 json_metadata = {}
-
-
-def get_config_data(path):
-    data = None
-    with open(path, "r") as file:
-        data = yaml.safe_load(file)
-    return data
 
 
 def save_to_dataframe(text_list, category, df):
@@ -22,106 +16,97 @@ def save_to_dataframe(text_list, category, df):
     return pd.concat([df, data], ignore_index=True)
 
 
-def save_classification(config, random_neutral, random_medical_condition, df):
-    df = save_to_dataframe(random_neutral, 0, df)
-    df = save_to_dataframe(random_medical_condition, config["category"], df)
+def save_classification(config, neutral_texts, featured_texts, df):
+    df = save_to_dataframe(neutral_texts, 0, df)
+    df = save_to_dataframe(featured_texts, config["category"], df)
 
     return df
 
 
-def classify_dataset(dataset, config):
-    neutral_text = []
-    medical_condition_text = []
+def split_dataset(dataset, config):
+    neutral_texts = []
+    featured_texts = []
     feature_tags = config["feature_presence_tags"]
 
     for _, row in dataset.iterrows():
         feature = row[config["feature_column"]]
-        if not isinstance(feature, (int, float, complex)):
-            feature = feature.strip()
+        if isinstance(feature, int) or isinstance(feature, float):
+            if math.isnan(feature):
+                continue
+            feature = str(int(feature)).strip()
 
-        text_column = row[config["text_column"]]
-        medical_condition_text.append(
-            text_column
-        ) if feature in feature_tags else neutral_text.append(text_column)
+        txt = row[config["text_column"]]
 
-    return neutral_text, medical_condition_text
+        if feature in feature_tags:
+            featured_texts.append(txt)
+        else:
+            neutral_texts.append(txt)
 
-
-def save_to_json(json_obj, utc_datetime):
-    file_name = f"./metrics/metrics__{utc_datetime}.json"
-    os.makedirs(os.path.dirname(file_name), exist_ok=True)
-    with open(file_name, "w") as json_file:
-        json.dump(json_obj, json_file, indent=4)
+    return neutral_texts, featured_texts
 
 
-def save_to_tsv(df, utc_datetime):
-    file_name = f"./datasets/output__{utc_datetime}.tsv"
-    os.makedirs(os.path.dirname(file_name), exist_ok=True)
-    df.to_csv(file_name, sep="\t", index=False)
-
-
-def save(df):
+def save_dataset(df):
     utc_datetime = datetime.utcnow()
     utc_datetime_str = utc_datetime.strftime("%Y-%m-%d_%H-%M-%S")
     json_obj = {"categories": json_categories, "metadata": json_metadata}
-    save_to_tsv(df, utc_datetime_str)
-    save_to_json(json_obj, utc_datetime_str)
 
+    output_dir_path = os.path.join(os.getcwd(), f"output-{utc_datetime_str}")
+    os.makedirs(output_dir_path, exist_ok=True)
 
-def save_metrics(category, tag, dataset_name, metadata):
-    json_categories[category] = tag
-    json_metadata[dataset_name] = metadata
+    with open(os.path.join(output_dir_path, "metadata.json"), "w") as json_file:
+        json.dump(json_obj, json_file, indent=4)
 
-
-def generate_metadata(
-    neutral_size, medical_condition_size, neutral_text, medical_condition_text
-):
-    return {
-        "neutral": neutral_size,
-        "featured": medical_condition_size,
-        "total_neutral": len(neutral_text),
-        "total_featured": len(medical_condition_text),
-    }
+    df.to_csv(os.path.join(output_dir_path, "dataset.tsv"), sep="\t", index=False)
 
 
 def generate_dataset(config_datasets):
     df = pd.DataFrame(columns=["text", "category"])
 
     for dataset_name, config in config_datasets["datasets"].items():
-        config = config_datasets["datasets"][dataset_name]
-        dataset = pd.read_csv(config["url"])
-        neutral_text, medical_condition_text = classify_dataset(dataset, config)
+        dataset = pd.read_csv(config["url"], sep="\t")
+        neutral_texts, featured_texts = split_dataset(dataset, config)
 
-        neutral_size = int(len(neutral_text) * config["proportion_neutral"])
-        medical_condition_size = int(
-            len(medical_condition_text) * config["proportion_with_feature"]
+        neutral_size = round(len(neutral_texts) * config["proportion_neutral"])
+        featured_size = round(
+            len(featured_texts) * config["proportion_with_feature"]
         )
 
         df = save_classification(
             config,
-            neutral_text[:neutral_size],
-            medical_condition_text[:medical_condition_size],
+            neutral_texts[:neutral_size],
+            featured_texts[:featured_size],
             df,
         )
 
-        metadata = generate_metadata(
-            neutral_size, medical_condition_size, neutral_text, medical_condition_text
-        )
-        save_metrics(config["category"], config["label"], dataset_name, metadata)
+        metadata = {
+            "neutral": neutral_size,
+            "featured": featured_size,
+            "total_neutral": len(neutral_texts),
+            "total_featured": len(featured_texts),
+        }
 
-    save(df)
+        json_categories[config["category"]] = config["label"]
+        json_metadata[dataset_name] = metadata
+
+    save_dataset(df)
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description="Generate general dataset")
-    parser.add_argument("-c", help="path yaml configuration", type=str)
+    parser.add_argument(
+        "-c", help="path to yaml configuration", required=True, type=str
+    )
 
     return parser.parse_args()
 
 
 def main():
     args = create_parser()
-    config_datasets = get_config_data(args.c)
+
+    config_datasets = None
+    with open(args.c, "r") as file:
+        config_datasets = yaml.safe_load(file)
+
     generate_dataset(config_datasets)
 
 
