93be53fee453a4bc03820fde4af32aee909be993
sebashack <sebastian0092@gmail.com>
2024-05-14 20:57:38 +0100

    Contrain text size + add more useful metadata + some useful logs

--- distil-bert-dataset/src/__main__.py
+++ distil-bert-dataset/src/__main__.py
@@ -24,11 +24,16 @@ def save_classification(config, neutral_texts, featured_texts, df):
     return df
 
 
-def split_dataset(dataset, config):
+def count_words(text):
+    return len(text.split())
+
+
+def split_dataset(dataset, max_word_length, config):
     neutral_texts = []
     featured_texts = []
     feature_tags = [s.strip() for s in config["feature_presence_tags"]]
 
+    discarded = 0
     for _, row in dataset.iterrows():
         feature = row[config["feature_column"]]
         if isinstance(feature, int) or isinstance(feature, float):
@@ -45,10 +50,17 @@ def split_dataset(dataset, config):
 
         txt = txt.replace("\t", " ")
 
+        if count_words(txt) > max_word_length:
+            discarded += 1
+            continue
+
         if feature in feature_tags:
             featured_texts.append(txt)
         else:
             neutral_texts.append(txt)
+
+    if discarded > 0:
+        print(f"{discarded} text(s) exceeded max word size {max_word_length}")
 
     random.shuffle(neutral_texts)
     random.shuffle(featured_texts)
@@ -56,10 +68,14 @@ def split_dataset(dataset, config):
     return neutral_texts, featured_texts
 
 
-def save_dataset(df):
+def save_dataset(df, counts):
     utc_datetime = datetime.utcnow()
     utc_datetime_str = utc_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-    json_obj = {"categories": json_categories, "metadata": json_metadata}
+    json_obj = {
+        "categories": json_categories,
+        "metadata": json_metadata,
+        "counts": counts,
+    }
 
     output_dir_path = os.path.join(os.getcwd(), f"output-{utc_datetime_str}")
     os.makedirs(output_dir_path, exist_ok=True)
@@ -72,10 +88,14 @@ def save_dataset(df):
 
 def generate_dataset(config_datasets):
     df = pd.DataFrame(columns=["text", "category"])
+    max_word_length = config_datasets["max_word_length"]
+
+    counts = {"neutral": 0}
 
     for dataset_name, config in config_datasets["datasets"].items():
+        print(f"Processing dataset: {dataset_name}")
         dataset = pd.read_csv(config["url"], sep="\t")
-        neutral_texts, featured_texts = split_dataset(dataset, config)
+        neutral_texts, featured_texts = split_dataset(dataset, max_word_length, config)
 
         neutral_size = round(len(neutral_texts) * config["proportion_neutral"])
         featured_size = round(len(featured_texts) * config["proportion_with_feature"])
@@ -87,20 +107,27 @@ def generate_dataset(config_datasets):
             df,
         )
 
+        label = config["label"]
         metadata = {
             "neutral": neutral_size,
             "featured": featured_size,
             "total_neutral": len(neutral_texts),
             "total_featured": len(featured_texts),
-            "label": config["label"],
+            "label": label,
             "dataset": dataset_name,
             "dataset_url": config["url"],
         }
 
-        json_categories[config["category"]] = config["label"]
+        json_categories[config["category"]] = label
         json_metadata[dataset_name] = metadata
 
-    save_dataset(df)
+        counts["neutral"] += neutral_size
+        if config["label"] in counts:
+            counts[label] += featured_size
+        else:
+            counts[label] = featured_size
+
+    save_dataset(df, counts)
 
 
 def create_parser():
